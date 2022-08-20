7447919d11b918e62f0595292c9466dbda4b021f
Chaeyoung Shin <87316705+scy0723@users.noreply.github.com>
2022-08-20 15:34:51 +0900

    0820

---
+++ Libft-main/libft/ft_lstmap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chashin <chashin@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/07 15:50:40 by chashin           #+#    #+#             */
+/*   Updated: 2022/08/07 15:50:41 by chashin          ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*tmp;
+	t_list	*res;
+
+	if (!lst)
+		return (NULL);
+	res = NULL;
+	while (lst)
+	{
+		tmp = ft_lstnew(f(lst->content));
+		if (!tmp)
+		{
+			ft_lstclear(&lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&res, tmp);
+		lst = lst->next;
+	}
+	return (res);
+}
---
+++ Libft-main/libft/ft_memset.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chashin <chashin@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/07 15:50:55 by chashin           #+#    #+#             */
+/*   Updated: 2022/08/07 15:50:56 by chashin          ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < len)
+	{
+		((unsigned char *)b)[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
